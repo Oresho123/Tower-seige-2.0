f5bb5dbcb6db5e2a0f354e828daac81677d15d61
Oresho123 <70355271+Oresho123@users.noreply.github.com>
2020-10-22 16:40:38 -0600

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,42 @@
+class Polygon{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+            
+            pop();
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Tower-seige-2.0
+# p5.play-boilerplate
+Boiler plate for p5.play
